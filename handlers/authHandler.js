7d7504fad61bf68b2877974eef94fbde44db130b
Alex Black <wa7@sa34.cx>
2021-05-14 20:49:47 +0300

    Auth Start

--- handlers/authHandler.js
+++ handlers/authHandler.js
@@ -1,1 +1,13 @@
 import User from './../models/userModel.js'
+import catchAsync from './../utils/catchAsync.js'
+
+export const signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body)
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  })
+})
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -8,7 +8,12 @@ import {
   deleteUser,
 } from './../handlers/usersHandler.js'
 
+import { signup } from './../handlers/authHandler.js'
+
 const router = express.Router()
+
+router.post('/signup', signup)
+// router.post('/login', login)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser)
